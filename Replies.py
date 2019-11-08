fe7e27be94ad326703bc35af634f1b3e8b6b30fb
thecoder921 <54221295+thecoder921@users.noreply.github.com>
2019-11-08 14:52:29 +0530

    Add files via upload

---
+++ Replies.py
@@ -0,0 +1,103 @@
+pairs = [
+    [
+        r"my name is (.*)",
+        ["Hello %1, How are you today ?",]
+    ],
+     [
+        r"what is your name ?",
+        ["My name is Chatter and I'm a chatbot ?",]
+    ],
+    [
+        r"I am (Fine|doing good|Awesome|at my best)",
+        ["Nice to hear that","Alright :)",]
+    ],
+    [
+        r"I (.*) (ill|bad|crying|Depressed|in a bad mood)",
+        ["Everything will be fine bro","Why,What happened dude?",]
+    ],
+    [
+        r"I am (.*)",
+        ["Hello %1, How are you today ?",]
+    ],
+    [
+        r"how are you ?",
+        ["I'm doing good\nHow about You ?",]
+    ],
+    [
+        r"sorry (.*)",
+        ["Its alright","Its OK, never mind",]
+    ],
+    [
+        r"i'm (.*) doing good",
+        ["Nice to hear that","Alright :)",]
+    ],
+    [
+        r"hi|hey|hello",
+        ["Hello", "Hey there",]
+    ],
+    [
+        r"(.*) age?",
+        ["I'm a computer program dude\nSeriously you are asking me this?",]
+        
+    ],
+    [
+        r"what (.*) want ?",
+        ["Make me an offer I can't refuse","I want a PlayStation 4",]
+        
+    ],
+    [
+        r"who(.*) created you ?",
+        ["Priyanshu Patidar created me using Python's NLTK Library","top secret ;)","Who Am I to tell you that",]
+    ],
+    [
+        r"(.*) (location|city) ?",
+        ['I am Omipotent',]
+    ],
+    [
+        r"how (.*) weather in (.*)?",
+        ["Weather in %2 is awesome like always","Too hot man here in %2","Too cold man here in %2","Never even heard about %2"]
+    ],
+    [
+        r"i work in (.*)?",
+        ["%1 is an Amazing company, I have heard about it. But they are in huge loss these days.",]
+    ],[
+        r"(.*)raining in (.*)",
+        ["Pakore Mil Jate to maja hi aa jata","No rain since last week here in %2","Damn its raining too much here in %2"]
+    ],
+    [
+        r"how (.*) health(.*)",
+        ["I'm a computer program, so I'm always healthy ","It depends on your Machines Battery dude,LOL"]
+    ],
+    [
+        r"who (.*) Sportsperson?",
+        ["Sachin Tendulkar","Rohit Sharma","Mahi(MSD)"]],
+    [
+        r"(.*) (sport|game) ?",
+        ["I'm a very big fan of Cricket",]
+    ],
+    [
+        r"who is your(.*) (moviestar|actor)?",
+        ["Akshay Kumar"]],
+    [
+        r"which (.*) movie?",
+        ["Baahubali-The Conclusion"]],
+    [
+        r"(quit|bye|Bye)",
+        ["BBye take care. See you soon :) ","It was nice talking to you. See you soon :)"]],
+    [
+        r"who (.*) you ?",
+        ["I'm a chatbot ?",]
+    ],
+    [
+        r"nice",
+        ['So You are Priyansh Tiwari,Am I Correct?']],
+    [
+        r"how (.*) priyansh Tiwari",
+        ["Because you are unsatisfied bro..."]],
+     [
+        r"who (.*) Priyanshu Patidar",
+        ["He is My Creator"]],
+    [
+        r"(.*)",
+        ['I am Not a much developed Program but you can help me get more advanced by contacting on "priyanshupatidar@yahoo.com"']],
+]
---
+++ nltkchtbot.py
@@ -0,0 +1,4 @@
+from nltk.chat.util import Chat,reflections
+from Replies import pairs
+chat=Chat(pairs,reflections)
+chat.converse()
